833b4966f2ee890c5cca062f85d8dc3ad2645960
ipopop <popop@ntymail.com>
2023-02-27 15:33:54 +0100

    add terre01.js

---
+++ terre01.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Create a program that displays its file name.
+// Langage : Javascript
+// Exec Command in Shell : node terre01.js
+
+console.log(__filename.replace(/^.*[\\\/]/, ''));
