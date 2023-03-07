add5305057803b4fe91826829bbe1e261f77cd7b
ipopop <popop@ntymail.com>
2023-03-07 14:35:28 +0100

    add terre08.cs

---
+++ terre08.cs
@@ -0,0 +1,15 @@
+// Display the result of a power.
+// The exponent cannot be negative.
+// Caution: manage potential argument errors.
+// Langage : C#
+// Exec Command in Shell : csc terre08.cs && mono terre08.exe
+
+using System;
+
+  class Program
+  {
+    static void Main(String[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
