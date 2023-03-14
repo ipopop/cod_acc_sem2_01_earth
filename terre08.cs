2779bad13f2dcbf087f5f1bce0c916306e5930e5
ipopop <popop@ntymail.com>
2023-03-14 04:40:34 +0100

    add terre08cs & terre09.jl

--- terre08.cs
+++ terre08.cs
@@ -6,10 +6,37 @@
 
 using System;
 
-  class Terre08
-  {
-    static void Main(String[] args)
-    {
-      Console.WriteLine("Hello World!");
+class Program {
+    static void Main(string[] args) {
+        int baseNum, exponent, result;
+
+        // Prompt user to enter the base number
+        Console.Write("Enter the base number: ");
+
+        // Read the base number from user input and convert it to an integer
+        baseNum = int.Parse(Console.ReadLine());
+
+        // Prompt user to enter the exponent
+        Console.Write("Enter the exponent: ");
+
+        // Read the exponent from user input and convert it to an integer
+        exponent = int.Parse(Console.ReadLine());
+
+        // Check if exponent is negative
+        if (exponent < 0) {
+            Console.WriteLine("Exponent cannot be negative.");
+
+            // Exit the program if exponent is negative
+            return;
+        }
+
+        // Calculate power using a loop
+        result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result *= baseNum;
+        }
+
+        // Display the result
+        Console.WriteLine($"{baseNum} raised to the power of {exponent} is {result}");
     }
-  }
+}
